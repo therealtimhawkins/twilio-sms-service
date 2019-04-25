9b0f8db963a26692805c0bf4145e4d950365b218
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-25 12:56:56 +0100

    removes sensitive number from smsHandler

--- src/handlers/smsHandler.js
+++ src/handlers/smsHandler.js
@@ -1,11 +1,12 @@
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_AUTH;
+const twilioNumber = process.env.TWILIO_NUMBER;
 const client = require('twilio')(accountSid, authToken);
 
 const sendSmsMessage = (message, number) => {
   return client.messages
     .create({
-      from: `+447480802881`,
+      from: twilioNumber,
       body: message,
       to: number
     })
