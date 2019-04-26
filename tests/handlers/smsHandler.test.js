cc9e87f02d2cb2f12707011a73871ab8df52fd86
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-26 12:51:07 +0100

    adds jest to project with first test

---
+++ tests/handlers/smsHandler.test.js
@@ -0,0 +1,5 @@
+const { sendSmsMessage } = require('../../src/handlers/smsHandler');
+
+test('sendSmsMessage sends message when phone number and message given', () => {
+  expect(true).toEqual(true);
+});
