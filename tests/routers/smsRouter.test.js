6fcfbe63b8d4516b92da974bb8e2b124e92ac15c
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-28 21:22:15 +0100

    adds test file for smsRouter

---
+++ tests/routers/smsRouter.test.js
@@ -0,0 +1,7 @@
+const { router } = require('../../src/routers/smsRouter');
+
+describe('smsRouter', () => {
+  test('get /number/:number/message/:message', () => {
+    expect(true).toEqual(true);
+  });
+});
