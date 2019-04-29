4921acb941d63b0d54efc18b282267fce23f4942
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-29 16:29:14 +0100

    adds integration test for twilio route

--- tests/routers/smsRouter.test.js
+++ tests/routers/smsRouter.test.js
@@ -1,7 +1,10 @@
-const { router } = require('../../src/routers/smsRouter');
+const app = require('../../index'); 
+const request = require('supertest');
 
 describe('smsRouter', () => {
-  test('get /number/:number/message/:message', () => {
-    expect(true).toEqual(true);
+  test('get /number/:number/message/:message', (done) => {
+    request(app)
+      .get(`/sms/number/${process.env.TWILIO_TEST_NUMBER}/message/TestingMessage`)
+      .expect(200, done);
   });
 });
