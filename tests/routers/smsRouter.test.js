b02944bfd313639880f99ef0edb6d0b39d974061
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-29 16:46:34 +0100

    updates test for resilience

--- tests/routers/smsRouter.test.js
+++ tests/routers/smsRouter.test.js
@@ -5,6 +5,11 @@ describe('smsRouter', () => {
   test('get /number/:number/message/:message', (done) => {
     request(app)
       .get(`/sms/number/${process.env.TWILIO_TEST_NUMBER}/message/TestingMessage`)
-      .expect(200, done);
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        expect(response.body.body).toEqual('Sent from your Twilio trial account - TestingMessage');
+        done();
+      });
   });
 });
