fb3b07e09281955ffa1d99912afb35459137eb89
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-30 10:33:17 +0100

    updates twilio integration test

--- tests/routers/smsRouter.test.js
+++ tests/routers/smsRouter.test.js
@@ -4,11 +4,11 @@ const request = require('supertest');
 describe('smsRouter', () => {
   test('get /number/:number/message/:message', (done) => {
     request(app)
-      .get(`/sms/number/${process.env.TWILIO_TEST_NUMBER}/message/TestingMessage`)
+      .get(`/sms/number/${process.env.TWILIO_TEST_NUMBER}/message/Testing%20Message`)
       .expect('Content-Type', /json/)
       .expect(200)
       .then(response => {
-        expect(response.body.body).toEqual('Sent from your Twilio trial account - TestingMessage');
+        expect(response.body.body).toEqual('Sent from your Twilio trial account - Testing Message');
         done();
       });
   });
